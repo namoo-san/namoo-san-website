51c73c46b3cf5764fc13505926e20eba92a4774c
namoo-san <highgraphics.x@gmail.com>
2019-02-22 18:10:38 +0900

    zap

--- components/nav.vue
+++ components/nav.vue
@@ -45,7 +45,7 @@
       <div class="navbar-item">
         <div class="field is-grouped">
           <p class="control">
-            <a class="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="http://localhost:3000" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=http://localhost:4000&amp;via=jgthms">
+            <a class="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="http://localhost:3000" target="_blank" href="https://twitter.com/intent/tweet?text=Nuxt.js+Netlify=⚡">
               <span class="icon">
                 <i class="fab fa-twitter"></i>
               </span>
