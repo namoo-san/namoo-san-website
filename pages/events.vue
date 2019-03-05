d198958678d7392e5c7277a9f6741dc88f203d20
namoo-san <highgraphics.x@gmail.com>
2019-03-05 13:52:47 +0900

    Events page

--- pages/events.vue
+++ pages/events.vue
@@ -1,41 +1,189 @@
 <template>
-<article>
+  <article>
     <navbar/>
-<div class="card">
-  <div class="card-image">
-    <figure class="image is-4by3">
-      <img src="https://images.pexels.com/photos/529930/pexels-photo-529930.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="DJ Image">
-    </figure>
-  </div>
-  <div class="card-content">
-    <div class="media">
-      <div class="media-left">
-        <figure class="image is-48x48">
-          <img src="~/static/logoimg.jpg" alt="Logo Image">
-        </figure>
-      </div>
-      <div class="media-content">
-        <p class="title is-4">DJ Works</p>
-        <p class="subtitle is-6">ALL EVENTS</p>
-      </div>
-    </div>
-    <div class="content">
-      <div>
-        好きなジャンル：
-        <div class="color">
-          #House #FunkyHouse #DiscoHouse #Jackin'House #DeepHouse #FutureHouse #EDM
+    <div class="columns">
+      <div class="column">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img class="imgfix" src="~/static/ub.jpg" alt="DJ Image">
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">UNBOXX #1 #2 #3</p>
+                <p class="subtitle is-6">Shibuya - 円山町</p>
+              </div>
+            </div>
+            <div class="content">
+              <div>
+                Open :
+                <div class="color">
+                  Every Friday / 19:30
+                </div>
+              </div>
+              <div>
+                Concept :
+                <div class="color">
+                  All Genre B2B
+                </div>
+              </div>
+              <div>
+                Mixed :
+                <div class="color">
+                  #R&B #Techno #House #J-POP #Trance #HardDance
+                </div>
+              </div>
+              <div class="subtitle is-5">Event Finished 🙇</div>
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img class="imgfix" src="~/static/ubl.jpg" alt="DJ Image">
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">UNBOXX lounge #1</p>
+                <p class="subtitle is-6">Shibuya - 円山町</p>
+              </div>
+            </div>
+            <div class="content">
+              <div>
+                Open :
+                <div class="color">
+                  Every Friday / 19:30
+                </div>
+              </div>
+              <div>
+                Concept :
+                <div class="color">
+                  All Genre Lounge
+                </div>
+              </div>
+              <div>
+                Mixed :
+                <div class="color">
+                  #House #R&B #HipHop
+                </div>
+              <div class="subtitle is-5">Event Finished 🙇</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div>
-        ミックスジャンル：
-        <div class="color">
-          #R&B #Techno #HappyCore #DubStep #GlitchHop #HipHop
+      <div class="column">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img class="imgfix" src="~/static/wl.jpg" alt="DJ Image">
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">WeekenderLounge</p>
+                <p class="subtitle is-6">Shibuya - 円山町</p>
+              </div>
+            </div>
+            <div class="content">
+              <div>
+                Open :
+                <div class="color">
+                  Every Friday / 19:30
+                </div>
+              </div>
+              <div>
+                Concept :
+                <div class="color">
+                  Lounge music
+                </div>
+              </div>
+              <div>
+                Mixed :
+                <div class="color">
+                  #HipHop #R&B #House #DeepHouse
+                </div>
+              <div class="subtitle is-5">Event Finished 🙇</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img class="imgfix" src="~/static/noimage.jpg" alt="DJ Image">
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">Party Vol.1</p>
+                <p class="subtitle is-6">Roppongi - Cube</p>
+              </div>
+            </div>
+            <div class="content">
+              <div>
+                Concept :
+                <div class="color">
+                  All genre party
+                </div>
+              </div>
+              <div>
+                Mixed :
+                <div class="color">
+                  #House #Techno #J-POP
+                </div>
+              </div>
+              <div class="subtitle is-5">Event Finished 🙇</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img class="imgfix" src="~/static/noimage.jpg" alt="DJ Image">
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">Reception Party</p>
+                <p class="subtitle is-6">Akasaka</p>
+              </div>
+            </div>
+            <div class="content">
+              <div>
+                Open :
+                <div class="color">
+                  Feb 3 / 19:00
+                </div>
+              </div>
+              <div>
+                Concept :
+                <div class="color">
+                  Art reception event
+                </div>
+              </div>
+              <div>
+                Mixed :
+                <div class="color">
+                  #HipHop #R&B #House
+                </div>
+              <div class="subtitle is-5">Event Finished 🙇</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
-</article>
+  </article>
 </template>
 
 <script>
@@ -46,7 +194,7 @@ export default {
     navbar
   },
   head: {
-    title: '🎧'
+    title: '🎧 + 🔧'
   }
 }
 </script>
@@ -55,8 +203,15 @@ export default {
 .card{
   margin: 4%;
 }
+.sections{
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
 .color{
-  color: #00D1B2;
+  color: rgb(0, 162, 138);
   display: inline;
+}
+.imgfix{
+  object-fit: cover;
 }
 </style>
