ac75f053065ad73cb20fbf772c957be8a42e92bd
namoo-san <highgraphics.x@gmail.com>
2019-03-05 13:53:15 +0900

    🔧 renamed to works

---
+++ pages/works.vue
@@ -0,0 +1,123 @@
+<template>
+<article>
+    <navbar/>
+    <div class="columns">
+      <div class="column">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="https://images.pexels.com/photos/529930/pexels-photo-529930.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="DJ Image">
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-48x48">
+                  <img src="~/static/nimg.jpg" alt="Logo Image">
+                </figure>
+              </div>
+              <div class="media-left">
+                <figure class="image is-48x48">
+                  <img src="~/static/cimg.jpg" alt="Logo Image">
+                </figure>
+              </div>
+              <div class="media-content">
+                <p class="title is-4">DJ Works</p>
+                <p class="subtitle is-6">ALL EVENTS</p>
+              </div>
+            </div>
+            <div class="content">
+              <div>
+                Like :
+                <div class="color">
+                  #House #FunkyHouse #DiscoHouse #Jackin'House #DeepHouse #FutureHouse #EDM
+                </div>
+              </div>
+              <div>
+                Mix :
+                <div class="color">
+                  #R&B #Techno #HappyCore #DubStep #GlitchHop #HipHop
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="DJ Image">
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-48x48">
+                  <img src="~/static/logoimg.jpg" alt="Logo Image">
+                </figure>
+              </div>
+              <div class="media-content">
+                <p class="title is-4">Development</p>
+                <p class="subtitle is-6">@namoo-san</p>
+              </div>
+            </div>
+            <div class="content">
+              <div class="sections">
+                Infrastructure - AWS
+                <div class="color">
+                  #EC2 #VPC #S3 #Amazon-Connect
+                </div>
+              </div>
+              <div class="sections">
+                Infrastructure - Azure
+                <div class="color">
+                  #Azure-Active-Directory #Azure-MFA #VM #Azure-Files #Blob-Storage #Conditional-Access
+                </div>
+              </div>
+              <div class="sections">
+                Web Technologies
+                <div class="color">
+                  #HTML #CSS #Node.js #Nuxt.js #Netlify
+                </div>
+              </div>
+              <div class="sections">
+                Other Skills
+                <div class="color">
+                  #Python3 #Shell #zsh #GSuite-Admin #Slack-Admin #Active-Directory #MDM #CentOS6 #CentOS7 #Servers #pm2
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</article>
+</template>
+
+<script>
+import navbar from '~/components/nav.vue'
+
+export default {
+  components: {
+    navbar
+  },
+  head: {
+    title: '🎧 + 🔧'
+  }
+}
+</script>
+
+<style>
+.card{
+  margin: 4%;
+}
+.sections{
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+.color{
+  color: rgb(0, 162, 138);
+  display: inline;
+}
+</style>
