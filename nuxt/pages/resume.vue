4b9e3a96c8cd88eb8543b7b03a99ef668eb17ea9
namoo-san <highgraphics.x@gmail.com>
2019-10-11 13:23:52 +0900

    Add resume

---
+++ nuxt/pages/resume.vue
@@ -0,0 +1,98 @@
+<template>
+<article>
+  <section class="hero is-light">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          Resume
+        </h1>
+        <h2 class="subtitle">
+          & Experience
+        </h2>
+      </div>
+    </div>
+  </section>
+  <section class="section">
+  <div class="columns is-mobile">
+    <div class="column">
+      <div class="content has-text-centered">
+      <table class="table is-striped">
+        <thead>
+          <tr>
+            <th>Period</th>
+            <th>Company</th>
+            <th>Jobs</th>
+            <th>Skills</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>2011-2012</th>
+            <td>ダーツリース会社</td>
+            <td>情報システム</td>
+            <td>ネットワーク/サーバー構築/IT機器コンサル/オフィス構築</td>
+          </tr>
+          <tr>
+            <th>2012-2013</th>
+            <td></td>
+            <td>19</td>
+            <td>9</td>
+          </tr>
+          <tr>
+            <th>2013-2016</th>
+            <td>フリーランス</td>
+            <td>デザイナー</td>
+            <td>Photoshop/Illustrator/InDesign/Dreamweaver</td>
+          </tr>
+          <tr>
+            <th>-</th>
+            <td>フリーランス</td>
+            <td>DJ</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <th>-</th>
+            <td>フリーランス</td>
+            <td>VJ</td>
+            <td>Modul8/AfterEffects/PremierPro/resolume/QuartzComposer</td>
+          </tr>
+          <tr>
+            <th>-</th>
+            <td>フリーランス</td>
+            <td>ITコンサル</td>
+            <td>飲食店向けIT機器コンサル/設備導入</td>
+          </tr>
+          <tr class="is-selected">
+            <th>2016-2019</th>
+            <td>38</td>
+            <td>19</td>
+            <td>9</td>
+          </tr>
+        </tbody>
+      </table>
+      </div>
+    </div>
+  </div>
+  </section>
+</article>
+</template>
+
+<script>
+
+export default {
+  head: {
+    title: '🎧 + 🔧'
+  }
+}
+</script>
+
+<style>
+.sections{
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+.color{
+  color: rgb(0, 162, 138);
+  display: inline;
+}
+</style>
