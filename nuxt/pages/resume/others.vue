de767dedc9b9714f7aae11e09f56e91ec330e844
namoo-san <highgraphics.x@gmail.com>
2020-10-02 18:36:57 +0900

    Add page title, Add noindex

--- nuxt/pages/resume/others.vue
+++ nuxt/pages/resume/others.vue
@@ -114,8 +114,13 @@
 <script>
 export default {
   head: {
-    title: "🎧 + 🔧",
+    title: "nakashun.com - Others",
   },
+  meta: [
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { hid: "robots", name: "robots", content: "noindex" },
+  ],
 };
 </script>
 
