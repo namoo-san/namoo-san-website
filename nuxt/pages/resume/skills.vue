721799aefd666548e8891b2b98b23c20c1dce03a
namoo-san <highgraphics.x@gmail.com>
2020-10-06 00:02:46 +0900

    Add Skills

--- nuxt/pages/resume/skills.vue
+++ nuxt/pages/resume/skills.vue
@@ -2,12 +2,8 @@
   <article>
     <div class="hero-body">
       <div class="has-text-centered">
-        <p class="title">
-          💪
-        </p>
-        <p class="subtitle">
-          Skills
-        </p>
+        <p class="title">💪</p>
+        <p class="subtitle">Skills</p>
       </div>
     </div>
 
@@ -26,80 +22,92 @@
     </div>
 
     <section class="section">
-    <div class="columns">
-      <div class="column">
+      <div class="columns">
+        <div class="column">
           <article class="message is-warning">
             <div class="message-header">
               <p>Amazon Web Service</p>
-              
             </div>
             <div class="message-body">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              <strong>Pellentesque risus mi</strong>, tempus quis placerat ut,
-              porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla.
-              Nullam gravida purus diam, et dictum
-              <a>felis venenatis</a> efficitur. Aenean ac
-              <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et
-              sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi
-              magna a neque. Donec dui urna, vehicula et sem eget, facilisis
-              sodales sem.
+              <div class="block">
+                AWSを利用して社内インフラ・サービスインフラなどを構築した経験から、
+                主に下記のサービスを利用することが出来ます。
+              </div>
+              <div class="card-footer-item">
+                <div class="tags">
+                  <span class="tag is-warning">EC2</span>
+                  <span class="tag is-warning">Fargate</span>
+                  <span class="tag is-warning">ECS</span>
+                  <span class="tag is-warning">ECR</span>
+                  <span class="tag is-warning">VPC</span>
+                  <span class="tag is-warning">S3</span>
+                  <span class="tag is-warning">FSx</span>
+                  <span class="tag is-warning">Route 53</span>
+                  <span class="tag is-warning">CloudWatch Logs</span>
+                  <span class="tag is-warning">AWS Lambda</span>
+                  <span class="tag is-warning">IAM</span>
+                  <span class="tag is-warning">Amazon Connect</span>
+                  <span class="tag is-warning">Cost Explorer</span>
+                  <span class="tag is-warning">Workspaces</span>
+                  <span class="tag is-warning">Directory Service</span>
+                  <span class="tag is-warning">GuardDuty</span>
+                </div>
+              </div>
             </div>
           </article>
           <article class="message is-info">
             <div class="message-header">
               <p>Microsoft Azure</p>
-              
             </div>
             <div class="message-body">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              <strong>Pellentesque risus mi</strong>, tempus quis placerat ut,
-              porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla.
-              Nullam gravida purus diam, et dictum
-              <a>felis venenatis</a> efficitur. Aenean ac
-              <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et
-              sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi
-              magna a neque. Donec dui urna, vehicula et sem eget, facilisis
-              sodales sem.
+              <div class="block">
+                IdPの構築〜移行・セキュリティエリアのテナント構築等の経験から
+                下記Azureサービスを利用することができます。
+              </div>
+              <div class="card-footer-item">
+                <div class="tags">
+                  <span class="tag is-info">Azure Active Directory</span>
+                  <span class="tag is-info">Intune</span>
+                  <span class="tag is-info">Azure MFA</span>
+                  <span class="tag is-info">Conditional Access</span>
+                  <span class="tag is-info">Blob Storage</span>
+                  <span class="tag is-info">Azure Sentinel</span>
+                  <span class="tag is-light">Office 365 Business</span>
+                  <span class="tag is-light">Office 365 ProPlus</span>
+                  <span class="tag is-light">Microsoft Windows 10 E5</span>
+                  <span class="tag is-light">Enterprise Mobility & Security E5</span>
+                </div>
+              </div>
             </div>
           </article>
-      </div>
-      <div class="column">
-          <article class="message is-dark">
+        </div>
+        <div class="column">
+          <article class="message is-success">
             <div class="message-header">
-              <p>Coding skills</p>
-              
+              <p>Coding Skills</p>
             </div>
             <div class="message-body">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              <strong>Pellentesque risus mi</strong>, tempus quis placerat ut,
-              porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla.
-              Nullam gravida purus diam, et dictum
-              <a>felis venenatis</a> efficitur. Aenean ac
-              <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et
-              sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi
-              magna a neque. Donec dui urna, vehicula et sem eget, facilisis
-              sodales sem.
+              <div class="block">
+                ある言語のプロフェッショナル という訳ではありませんが、
+                手段に応じてコードを書きます。
+              </div>
+              <div class="card-footer-item">
+                <div class="tags">
+                  <span class="tag is-success">Python</span>
+                  <span class="tag is-success">JavaScript</span>
+                  <span class="tag is-success">Ruby</span>
+                  <span class="tag is-success">HashiCorp Configuration Language</span>
+                  <span class="tag is-success">PowerShell</span>
+                  <span class="tag is-success">Shell</span>
+                  <span class="tag is-success">Batch</span>
+                  <span class="tag is-success">HTML</span>
+                  <span class="tag is-success">CSS</span>
+                </div>
+              </div>
             </div>
           </article>
-          <article class="message is-dark">
-            <div class="message-header">
-              <p></p>
-              
-            </div>
-            <div class="message-body">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              <strong>Pellentesque risus mi</strong>, tempus quis placerat ut,
-              porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla.
-              Nullam gravida purus diam, et dictum
-              <a>felis venenatis</a> efficitur. Aenean ac
-              <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et
-              sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi
-              magna a neque. Donec dui urna, vehicula et sem eget, facilisis
-              sodales sem.
-            </div>
-          </article>
+        </div>
       </div>
-    </div>
     </section>
   </article>
 </template>
